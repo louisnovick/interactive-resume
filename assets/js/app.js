4a663b9f4dc929971abc4bfa447bb1bef6e801ed
louisnovick <louisnovick@hotmail.com>
2015-01-17 23:07:16 -0500

    started using svg instead of divs

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,45 @@ console.log('testing');
 
 function d3app() {
 
+	var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
+    /*
+    d3.select("body").selectAll("div")
+    	.data(dataset)
+    	.enter()
+    	.append("div")
+    	.attr("class", "bar")
+    	.style("height", function(d) {
+    		var barheight = d * 5;
+    		return barheight + "px";
+    	});
+	*/
+
+    var w = 500;
+    var h = 100;
+
+    var svg = d3.select("body")
+    	.append("svg")
+    	.attr("width", w)
+    	.attr("height", h);
+
+    svg.selectAll("rect")
+    	.data(dataset)
+    	.enter()
+    	.append("rect")
+    	.attr("x", function(d, i) {
+    		return i * (w / dataset.length);
+    	})
+    	.attr("y", 0)
+    	.attr("width", 20)
+    	.attr("height", 100);
+
+}
+
+
+
+	/*
 	var skills = [ "php", "js", "css", "html" ];
 	var skillarray = skills.length;
 
@@ -58,9 +97,4 @@ function d3app() {
 	.attr("r", function(d) {
 		return d;
 	});
-
-
-
-
-
-}
+*/
