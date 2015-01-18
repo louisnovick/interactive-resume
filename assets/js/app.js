2cd872852bf60d94f0e5a36b53f2be3d048a3131
louisnovick <louisnovick@hotmail.com>
2015-01-17 22:39:10 -0500

    continued work on d3

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,56 @@ function d3app() {
 		.append("p")
 		.text(function(d) { 
 			return "I can do " + d + "!"; 
+		})
+		.style("color", function(d) {
+			if (d > skills[3]) {
+				return "red";
+			} else {
+				return "blue";
+			}
 		});
+
+	var dataset = [];
+	for (var i = 0; i < 25; i++){
+		var newNumber = Math.round(Math.random() * 30);
+		dataset.push(newNumber);
+	}
+	console.log(dataset);
+
+	d3.select("body").selectAll("div")
+		.data(dataset)
+		.enter()
+		.append("div")
+		.attr("class", "bar")
+		.style("height", function(d) {
+			var barheight = d * 5;
+			return barheight + "px";
+		});
+
+	var w = 500;
+	var h = 50;
+	var svg = d3.select("body")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	var circles = svg.selectAll("circle")
+		.data(dataset)
+		.enter()
+		.append("circle");
+
+	circles.attr("cx", function(d, i) {
+		return (i * 50) + 25;
+	})
+	.attr("cy", h/2)
+	.attr("fill", "yellow")
+	.attr("stroke", "orange")
+	.attr("r", function(d) {
+		return d;
+	});
+
+
+
+
+
 }
